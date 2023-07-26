7209c5e5cefd7f35e79f269e720e6d410bc8bc73
MelroyNoronha <melmax1208@gmail.com>
2023-07-26 21:27:00 +0530

    Yup, next.js is solid!

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# Portfolio-site
-### A simple portfolio site hosted using GitHub Pages.
-You can visit the site  [here](https://melroynoronha.github.io/Portfolio-site/).
-
-
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
---
+++ pages/index.js
@@ -0,0 +1,53 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import Date from '../components/date';
+import {getSortedPostsData} from '../lib/posts'
+
+export function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({allPostsData}) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          I’m a creator at heart. 
+I stumbled into web and app development as a student and turned it into a career.
+
+I have a deep passion for music and have worked on projects like ‘Mel and the Dragon’ and ‘tronicDisease’.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+          </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
