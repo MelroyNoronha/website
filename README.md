6165dc1b6c6721e939a1320dbda8825c4ea4d029
Melroy Noronha <melmax1208@gmail.com>
2023-07-27 16:43:04 +0530

    Website redesign and revamp (#4)
    * delete old portfolio site
    
    * create react app
    
    * reset default create-react-app
    
    * Clean slate and new beginnings
    
    * Implement website redesign using Next.js
    
    Setup Next.js project.
    Implment home page.

--- README.md
+++
@@ -1,4 +0,0 @@
-# This is my website that's hosted using Github Pages.
-You can visit the site  [here](https://melroynoronha.github.io/my-website/).
-
-
---
+++ app/constants.ts
@@ -0,0 +1,7 @@
+export const urls = {
+  tronicDisease: "https://soundcloud.com/tronic-disease-202564065",
+  melAndTheDragon: "https://open.spotify.com/artist/2Rw8bBYLJDgxAqX8fWcFJ9?si=APQY59skTqyXlJCBvR0KSQ",
+  github: "https://github.com/MelroyNoronha",
+  blog: "#blog",
+  linkedIn: "https://www.linkedin.com/in/melroynoronha98/"
+}
