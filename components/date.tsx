19d970fa41bf4ee2dd2c216ef0b14f70b952471a
MelroyNoronha <melmax1208@gmail.com>
2023-08-03 18:28:57 +0530

    Improve styles, set metadata, semantic html

--- components/date.tsx
+++ components/date.tsx
@@ -1,6 +1,12 @@
 import { parseISO, format } from "date-fns";
 
+import styles from "./date.module.css";
+
 export default function Date({ dateString }: { dateString: string }) {
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "dd LLL yyyy")}</time>;
+  return (
+    <time className={styles.date} dateTime={dateString}>
+      {format(date, "dd LLL yyyy")}
+    </time>
+  );
 }
