2ccd84015d37a4ba8ee56e6c815e3b11cf8d1afc
MelroyNoronha <melmax1208@gmail.com>
2023-08-25 23:18:18 +0530

    Use rehype-external-links to open links in new tab
    - Use remark-rehype so we can use the rehype-external-links plugin to
      open blog links in a new tab.
    - Use rehype-sanitize to sanitize the HTML output since that is better suited for our use case than
      using jsdom + dompurify.

--- lib/purifyHtml.ts
+++
@@ -1,7 +0,0 @@
-import { JSDOM } from "jsdom";
-import DOMPurify from "dompurify";
-
-const window = new JSDOM("").window;
-
-export default (dangerousHtml: string) =>
-  DOMPurify(window).sanitize(dangerousHtml);
